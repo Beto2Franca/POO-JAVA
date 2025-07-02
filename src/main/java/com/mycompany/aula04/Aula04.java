9f50211d634f639ac7857fd66f490a805cd42305
beto franca <beto2franca@gmail.com>
2025-07-02 16:21:16 -0300

    metodo construtor

--- src/main/java/com/mycompany/aula04/Aula04.java
+++ src/main/java/com/mycompany/aula04/Aula04.java
@@ -3,9 +3,17 @@ package com.mycompany.aula04;
 
 public class Aula04 {
     public static void main(String[] args) {
-       Caneta c1 = new Caneta();
-       c1.setModelo("BIC");
-       c1.setPonta(0.7f);
+       Caneta c1 = new Caneta("BIC", "Amarelo", 0.4f);
        c1.status();
+       
+       Caneta c2 = new Caneta("KID", "AZUL", 1.9f);
+       c2.status();
+       
+       
+//       c1.setModelo("BIC");
+//       c1.setPonta(0.7f);
+//       c1.status();
+
+//        System.out.println("Tenho uma caneta " + c1.getModelo());
     }
 }
--- src/main/java/com/mycompany/aula04/Caneta.java
+++ src/main/java/com/mycompany/aula04/Caneta.java
@@ -3,6 +3,17 @@ package com.mycompany.aula04;
 public class Caneta {
     public String modelo;
     private float ponta;
+    private boolean tampada;
+    private String cor;
+    
+//    Metodo construtor
+    public Caneta(String m, String c, float p) {
+          this.modelo = m;  
+          this.cor = c;  
+          this.ponta = p;  
+          this.tampar();
+    }
+    
     
     public String getModelo(){
         return this.modelo;
@@ -19,9 +30,18 @@ public class Caneta {
         this.ponta = p;
     }
     
+    public void tampar(){
+        this.tampada = true;
+    }
+    public void destampar(){
+        this.tampada = false;
+    }
+    
     public void status(){
         System.out.println("Sobre a Caneta:");
-        System.out.println("Modelo: " + this.modelo);
-        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
     }
 }
